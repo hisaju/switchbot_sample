ac5774f3b80f3f31a281b8e252a1992c544ad713
hisaju <hisaju@gmail.com>
2021-09-05 16:59:27 +0900

    おやすみバージョン追加

--- app/controllers/alexas_controller.rb
+++ app/controllers/alexas_controller.rb
@@ -1,9 +1,18 @@
 class AlexasController < ApplicationController
-  def index
+  def create
     alexa = AlexaRuby.new(request.body.read)
     RoomLight.new.click
     AirConditioner.new.turn_on
     alexa.response.tell('おはようございます。朝の準備が出来ました。')
     render json: alexa.response.json
   end
+
+  def destroy
+    alexa = AlexaRuby.new(request.body.read)
+    RoomLight.new.click
+    AirConditioner.new.turn_off
+    alexa.response.tell('おやすみなさい。電気消しておきました。')
+    render json: alexa.response.json
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  post '/', to: 'alexas#index'
+  post '/', to: 'alexas#create'
+  post '/destroy', to: 'alexas#destroy'
 end
